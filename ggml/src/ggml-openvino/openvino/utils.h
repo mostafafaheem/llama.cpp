8373a599a4498f47b35f6e047dfa2a56a348b826
Mostafa Faheem <mostafaaafaheem@gmail.com>
2026-09-17 05:42:40 +0300

    OpenVINO Backend: Support common MTMD ops

--- ggml/src/ggml-openvino/openvino/utils.h
+++ ggml/src/ggml-openvino/openvino/utils.h
@@ -57,8 +57,9 @@ OutputVector rename_outputs_with_suffix(const OutputVector & outputs, const std:
 std::pair<ov::Output<Node>, ov::Output<Node>> make_sin_cos(int32_t * rope_params,
                                                            std::shared_ptr<ov::Node> inp_pos,
                                                            std::shared_ptr<ov::Node> rope_freqs_weight = nullptr,
-                                                           bool imrope = false,
-                                                           bool stateful = false);
+                                                           int mode = 0,
+                                                           bool stateful = false,
+                                                           int64_t head_dim = 0);
 
 ov::Output<ov::Node> process_view_input(const NodeContext & context, int input_index, int slice_len = 0, int axis = -1);
 
